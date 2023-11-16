fde981c3324ad10f7bb27cb9b7ae060120654caf
dotnetcsharp0x <dsotnikov1996@gmail.com>
2023-11-16 08:33:07 +0300

    add tickerlist endpoint

--- Skymey-main-gateaway/Controllers/StockController.cs
+++ Skymey-main-gateaway/Controllers/StockController.cs
@@ -2,10 +2,15 @@
 using Microsoft.Extensions.Options;
 using RestSharp;
 using Skymey_main_gateaway.Data;
+using Skymey_main_lib.Models.CryptoCurrentPricesView;
 using Skymey_main_lib.Models.Prices.CurrentPricesViewModel;
 using Skymey_main_lib.Models.Prices.StockPrices;
 using Skymey_main_lib.Models.Prices.StockPricesMongo;
+using Skymey_main_lib.Models.Tickers;
+using Skymey_main_lib.Models.Tickers.Polygon;
+using System.Net.Http.Json;
 using System.Text.Json;
+using static System.Net.WebRequestMethods;
 
 namespace Skymey_main_gateaway.Controllers
 {
@@ -41,6 +46,25 @@ namespace Skymey_main_gateaway.Controllers
                 var r = client.ExecuteAsync(request).Result.Content;
                 var userd = JsonSerializer.Deserialize<List<StockPricesMongo>>(r);
                 var ExchangesVM = (from i in userd select new CurrentPricesViewModel { Ticker = i.Ticker, Figi = i.Figi, Price = i.Price, Currency = i.Currency, Update = i.Update });
+                return Ok(ExchangesVM);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message + url);
+            }
+        }
+        [HttpGet]
+        [Route("GetShares")]
+        public async Task<IActionResult> GetShares()
+        {
+            string url = _optMongo.Value.Server + ":" + _optMongo.Value.Port;
+            try
+            {
+                HttpClient Http = new HttpClient();
+                var userd = await Http.GetFromJsonAsync<TickerList[]>(_optMongo.Value.Server+":"+ _optMongo.Value.Port + "/api/Stock/GetShares");
+                var ExchangesVM = (from i in userd select new SharesList { Ticker = i.ticker, Name = i.name, Market = i.market, Locale = i.locale, Type = i.type, Currency_name = i.currency_name,
+                Last_updated_utc = i.last_updated_utc, Composite_figi = i.composite_figi, Share_class_figi = i.share_class_figi, Primary_exchange = i.primary_exchange, Cik = i.cik, Update = i.Update});
+                Http.Dispose();
                 return Ok(ExchangesVM);
             }
             catch (Exception ex)
